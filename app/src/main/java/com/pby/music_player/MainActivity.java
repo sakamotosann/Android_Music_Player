f7fbbae3a13967ac8596d629694f832e1d0d5653
sakamotosann <15311937017@126.com>
2019-01-01 00:50:04 +0800

    2018-12-31 23:49:32

--- app/src/main/java/com/pby/music_player/MainActivity.java
+++ app/src/main/java/com/pby/music_player/MainActivity.java
@@ -1,13 +1,183 @@
 package com.pby.music_player;
 
+import android.animation.ObjectAnimator;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView musicStatus, musicTime, musicTotal;
+    private SeekBar seekBar;
+
+    private Button btnPlayOrPause, btnStop, btnQuit;
+    private SimpleDateFormat time = new SimpleDateFormat("mm:ss");
+
+    private boolean tag1 = false;
+    private boolean tag2 = false;
+    private MusicService musicService;
+
+    private void bindServiceConnection() {
+        Intent intent = new Intent(MainActivity.this, MusicService.class);
+        startService(intent);
+        bindService(intent, serviceConnection, this.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            musicService = ((MusicService.MyBinder) (service)).getService();
+            Log.i("musicService", musicService + "");
+            musicTotal.setText(time.format(musicService.mediaPlayer.getDuration()));
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicService = null;
+        }
+    };
+
+    public Handler handler = new Handler();
+    public Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            musicTime.setText(time.format(musicService.mediaPlayer.getCurrentPosition()));
+            seekBar.setProgress(musicService.mediaPlayer.getCurrentPosition());
+            seekBar.setMax(musicService.mediaPlayer.getDuration());
+            musicTotal.setText(time.format(musicService.mediaPlayer.getDuration()));
+            handler.postDelayed(runnable, 200);
+
+        }
+    };
+
+    private void findViewById() {
+        musicTime = findViewById(R.id.MusicTime);
+        musicTotal = findViewById(R.id.MusicTotal);
+        seekBar = findViewById(R.id.MusicSeekBar);
+        btnPlayOrPause = findViewById(R.id.BtnPlayorPause);
+        btnStop = findViewById(R.id.BtnStop);
+        btnQuit = findViewById(R.id.BtnQuit);
+        musicStatus = findViewById(R.id.MusicStatus);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById();
+        bindServiceConnection();
+        myListener();
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    musicService.mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    private void myListener() {
+        ImageView imageView = findViewById(R.id.Image);
+        final ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "rotation", 0f, 360.0f);
+        animator.setDuration(10000);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatCount(-1);
+        btnPlayOrPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (musicService.mediaPlayer != null) {
+                    seekBar.setProgress(musicService.mediaPlayer.getCurrentPosition());
+                    seekBar.setMax(musicService.mediaPlayer.getDuration());
+                }
+
+                if (!musicService.tag) {
+                    btnPlayOrPause.setText("暂停");
+                    musicStatus.setText("播放中");
+                    musicService.playOrPause();
+                    musicService.tag = true;
+
+                    if (!tag1) {
+                        animator.start();
+                        tag1 = true;
+                    } else {
+                        animator.resume();
+                    }
+                } else {
+                    btnPlayOrPause.setText("播放");
+                    musicStatus.setText("暂停");
+                    musicService.playOrPause();
+                    animator.pause();
+                    musicService.tag = false;
+                }
+
+                if (!tag2) {
+                    handler.post(runnable);
+                    tag2 = true;
+                }
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                musicStatus.setText("停止");
+                btnPlayOrPause.setText("播放");
+                musicService.stop();
+                animator.pause();
+                musicService.tag = false;
+            }
+        });
+
+        btnQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.removeCallbacks(runnable);
+                unbindService(serviceConnection);
+                Intent intent = new Intent(MainActivity.this, MusicService.class);
+                stopService(intent);
+                try {
+                    MainActivity.this.finish();
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(false);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
